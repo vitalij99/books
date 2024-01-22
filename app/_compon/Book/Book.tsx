d7b7611468f4798a8098b13dd0ffadecf0cfb183
vitalij99 <vitalisklymko@gmail.com>
2024-01-22 19:03:11 +0200

    add commit translate

--- app/_compon/Book/Book.tsx
+++ app/_compon/Book/Book.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Box, Link, Typography } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface BookProps {
   book: string[];
@@ -12,17 +12,26 @@ interface BookProps {
   };
 }
 
+// add translate
 const Book = ({ data }: { data: BookProps }) => {
   const [textBook, setTextBook] = useState(data.book);
 
+  useEffect(() => {
+    const textParagref = textBook.slice(0, 10);
+    console.log(textParagref);
+  }, [textBook]);
+
+  if (!data) {
+    return <div>Error</div>;
+  }
+
   return (
     <div>
-      {data &&
-        textBook.map((text, index) => {
-          return <Typography key={index}>{text}</Typography>;
-        })}
+      {textBook.map((text, index) => {
+        return <Typography key={index}>{text}</Typography>;
+      })}
 
-      <Box>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <Link href={data.nav.nextPage}>{data.nav.nextText}</Link>
         <Link href={data.nav.prevPage}>{data.nav.prevText}</Link>
       </Box>
