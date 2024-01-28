894026b419ef4bea71969ae249aa6d9f437482cc
vitalij99 <vitalisklymko@gmail.com>
2024-01-28 15:39:03 +0200

    add reader

---
+++ lib/reader.tsx
@@ -0,0 +1,38 @@
+export async function startReade({ book }: { book: string[] }) {
+  const synth = window.speechSynthesis;
+  const options = {
+    classDiv: 'content',
+    nextPage: 'nextchap',
+    language: 'Google español',
+    pitch: 2,
+    rate: 2,
+    reade: false,
+    timer: 2,
+    paragraf: 0,
+  };
+
+  const voices = synth.getVoices();
+
+  function speak() {
+    if (synth.speaking) {
+      console.error('speechSynthesis.speaking');
+      return;
+    } else if (!book) {
+      const utterThis = new SpeechSynthesisUtterance(...book);
+
+      utterThis.onerror = function () {
+        console.error('SpeechSynthesisUtterance.onerror');
+      };
+
+      for (let i = 0; i < voices.length; i++) {
+        if (voices[i].name === options.language) {
+          utterThis.voice = voices[i];
+          break;
+        }
+      }
+      utterThis.pitch = options.pitch;
+      utterThis.rate = options.rate;
+      synth.speak(utterThis);
+    }
+  }
+}
