fcf4113d366c1bdd580b8bfeb703f64d65684df2
vitalij99 <vitalisklymko@gmail.com>
2024-04-15 17:49:14 +0300

    add ReaderCard

---
+++ app/_compon/ReaderCard/ReaderCard.tsx
@@ -0,0 +1,64 @@
+import { Box, Button, Card, Slider, Typography } from '@mui/material';
+import React from 'react';
+import ReaderIcon from '../Reader/ReaderIcon';
+
+interface ReaderCardProps {
+  toggleDrawer: (
+    open: boolean
+  ) => (event: React.KeyboardEvent | React.MouseEvent) => void;
+  reader: any;
+
+  isreade: {
+    read: boolean;
+    pause: boolean;
+  };
+  maxParagraf: number;
+  handleReadeCancel: () => void;
+  handleChangeParagraf: (event: Event, value: any) => void;
+  handleReade: () => void;
+}
+
+const ReaderCard = ({
+  toggleDrawer,
+  reader,
+  isreade,
+  maxParagraf,
+  handleReadeCancel,
+  handleChangeParagraf,
+  handleReade,
+}: ReaderCardProps) => {
+  return (
+    <Card sx={{ padding: 1 }}>
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <Button onClick={toggleDrawer(true)}>
+          <ReaderIcon />
+        </Button>
+      </Box>
+
+      {reader && isreade.read && (
+        <Box>
+          <Button onClick={handleReade}>
+            <Typography>
+              {!isreade.read ? 'Старт' : isreade.pause ? 'Продовжити' : 'Пауза'}
+            </Typography>
+          </Button>
+          <Button onClick={handleReadeCancel}>
+            <Typography>Стоп</Typography>
+          </Button>
+          <Typography>Параграф</Typography>
+          <Slider
+            name="paragraf"
+            onChange={handleChangeParagraf}
+            min={0}
+            step={1}
+            max={maxParagraf}
+            value={reader?.paragraf}
+            valueLabelDisplay="auto"
+          />
+        </Box>
+      )}
+    </Card>
+  );
+};
+
+export default ReaderCard;
