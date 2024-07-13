8ce7c1970726e292d40c39427c60bbb5119a7a65
vitalij99 <vitalisklymko@gmail.com>
2024-07-13 08:51:27 +0300

    add setSaveBook

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,21 @@
 "use server"
-import { prisma } from "@/auth";
+import { auth, prisma } from "@/auth";
 
 
 export const getUsers = async () => {
   return await prisma.user.findMany()
+}
 
+export const setSaveBook = async () => {
+  const sesion = await auth()
+  console.log(sesion);
+  const data = {
+    title:"",
+    link: "",
+    chapter: 0,
+    image: "",
+    users: ""
+  }
+  const res = await prisma.books.create({data})
+  return res
 }
