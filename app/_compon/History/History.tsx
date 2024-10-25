ef0215e25e3b93acbae4395f321f5ecbd1bf89a7
vitalij99 <vitalisklymko@gmail.com>
2024-10-25 13:49:07 +0300

    style History

--- app/_compon/History/History.tsx
+++ app/_compon/History/History.tsx
@@ -1,9 +1,44 @@
 'use client';
-import { Box } from '@mui/material';
+import ItemList from '@/app/_compon/ItemList/ItemList';
+import { Box, Card, CardActions, CardContent, Link } from '@mui/material';
+
 import React from 'react';
 
+const pages = [
+  {
+    link: 'asd',
+    title: 'sade',
+  },
+  {
+    link: 'asd',
+    title: 'sade',
+  },
+  {
+    link: 'asd',
+    title: 'sade',
+  },
+  {
+    link: 'asd',
+    title: 'sade',
+  },
+];
+
 const History = () => {
-  return <Box>History</Box>;
+  return (
+    <Box>
+      <Card variant="outlined">
+        <CardContent>Відвідані книжки</CardContent>
+        <ItemList
+          items={pages}
+          renderItem={({ link, title }) => (
+            <CardActions>
+              <Link href={link}>{title}</Link>
+            </CardActions>
+          )}
+        />
+      </Card>
+    </Box>
+  );
 };
 
 export default History;
