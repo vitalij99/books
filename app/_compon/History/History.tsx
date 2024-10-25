5edd189a3a661b0459a1070acbd2f76ba5b3bc71
vitalij99 <vitalisklymko@gmail.com>
2024-10-25 13:14:41 +0300

    add History

---
+++ app/_compon/History/History.tsx
@@ -0,0 +1,9 @@
+'use client';
+import { Box } from '@mui/material';
+import React from 'react';
+
+const History = () => {
+  return <Box>History</Box>;
+};
+
+export default History;
--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import SaveBooksLinks from '@/app/_compon/SaveBooksLinks/SaveBooksLinks';
 import Container from '@/app/_compon/Container/Container';
+import History from '@/app/_compon/History/History';
+import { Box } from '@mui/material';
 
 const Books = async () => {
   return (
     <>
       <Container>
-        <SaveBooksLinks />
+        <Box sx={{ display: 'flex' }}>
+          <SaveBooksLinks />
+          <History />
+        </Box>
       </Container>
     </>
   );
