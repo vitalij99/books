7ac6a2730df234b6d0fc575b5cfe2b44b3ff79d8
vitalij99 <vitalisklymko@gmail.com>
2024-10-26 17:42:12 +0300

    add style History

--- app/_compon/History/History.tsx
+++ app/_compon/History/History.tsx
@@ -1,13 +1,22 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import { Box, Card, CardActions, CardContent, Link } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Link,
+  Typography,
+} from '@mui/material';
 import ItemList from '@/app/_compon/ItemList/ItemList';
 import { getStorageAr } from '@/lib/getStorage';
+import { getTimeHistoryDifference } from '@/lib/books';
 
 const BOOKS = [
   {
     link: '/',
     title: 'Тут будуть відвіданні книги',
+    time: new Date(),
   },
 ];
 
@@ -23,11 +32,18 @@ const History = () => {
     <Box>
       <Card variant="outlined">
         <CardContent>Відвіданні книжки</CardContent>
+        <CardContent sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography>Назва</Typography>
+          <Typography>Час потому</Typography>
+        </CardContent>
         <ItemList
           items={books}
-          renderItem={({ link, title }) => (
-            <CardActions>
+          renderItem={({ link, title, time }) => (
+            <CardActions
+              sx={{ display: 'flex', justifyContent: 'space-between' }}
+            >
               <Link href={link}>{title}</Link>
+              <Typography>{getTimeHistoryDifference(time)}</Typography>
             </CardActions>
           )}
         />
