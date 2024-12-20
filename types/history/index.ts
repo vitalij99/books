86c0ec9f2861736dc0bfdac2a63ffe6a7d3d3cab
vitalij99 <vitalisklymko@gmail.com>
2024-12-20 15:49:04 +0200

    add type history

---
+++ types/history/index.ts
@@ -0,0 +1,12 @@
+export type HistoryBook = {
+  link?: string;
+  title?: string;
+  time?: string;
+};
+export const HISTORY_BOOKS = [
+  {
+    link: '/',
+    title: 'Тут будуть відвіданні книги',
+    time: '00 хв',
+  },
+];
