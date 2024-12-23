dd4fd3573d8f64e63d23e1ec5d50375e3b99134c
vitalij99 <vitalisklymko@gmail.com>
2024-12-23 12:38:02 +0200

    fix error home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import { getBooksPopularAll } from '@/back';
-import Loader from '@/app/_compon/Loader/Loader';
+
 import ListBooksCard from '@/app/_compon/ListBooksCard/ListBooksCard';
 
 const popular = async () => {
   const books = await getBooksPopularAll();
 
   if (!books) {
-    return <Loader />;
+    return null;
   }
   return (
     <>
